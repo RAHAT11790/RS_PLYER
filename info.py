4671b5e1639085371be930ddb9a689eb43d00730
RAHAT11790 <rahatsarker224@gmail.com>
2025-12-14 14:19:23 +0600

    Upload info.py

---
+++ info.py
@@ -0,0 +1,40 @@
+import re
+from os import environ
+
+# Bot Session Name
+SESSION = environ.get('SESSION', 'rsanimelink')
+
+# Your Telegram Account Api Id And Api Hash
+API_ID = int(environ.get('API_ID', "25976192''))
+API_HASH = environ.get('API_HASH', "8ba23141980539b4896e5adbc4ffd2e2'')
+
+# Bot Token, This Is Main Bot
+BOT_TOKEN = environ.get('BOT_TOKEN', "8244184630:AAEZnsgIFmohZLlKxnbggjXHwNce6zOg0KU")
+
+# Admin Telegram Account Id For Withdraw Notification Or Anything Else
+ADMIN = int(environ.get('ADMIN', "6621572366''))
+
+# Back Up Bot Token For Fetching Message When Floodwait Comes
+BACKUP_BOT_TOKEN = environ.get('BACKUP_BOT_TOKEN', "8361715639:AAHqaP-a6J3nhGULjrYm8WD8WpcHvwQDcBQ")
+
+# Log Channel, In This Channel Your All File Stored.
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', "-1003525738568''))
+
+# Mongodb Database For User Link Click Count Etc Data Store.
+MONGODB_URI = environ.get("MONGODB_URI", "mongodb+srv://RAHAT1132:RAHAT11a@rahat.txn4lkk.mongodb.net/?appName=Rahat")
+
+# Stream Url Means Your Deploy Server App Url, Here You Media Will Be Stream And Ads Will Be Shown.
+STREAM_URL = environ.get("STREAM_URL", "")
+
+# This Link Used As Permanent Link That If Your Deploy App Deleted Then You Change Stream Url, So This Link Will Redirect To Stream Url.
+LINK_URL = environ.get("LINK_URL", "")
+
+# Others, Not Usefull
+PORT = environ.get("PORT", "8080")
+MULTI_CLIENT = False
+SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
+PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
+if 'DYNO' in environ:
+    ON_HEROKU = True
+else:
+    ON_HEROKU = False
